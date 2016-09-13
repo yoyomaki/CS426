a1df4baff68126c3e3c56f4add161099fbc5058f
Qian Qiao <qqiao@uwaterloo.ca>
2016-09-13 17:23:51 -0400

    qq: finished add_node & add_edge

--- A1/graph/graph.cpp
+++ A1/graph/graph.cpp
@@ -1,18 +1,35 @@
 uint64_t graph::add_node(uint64_t node_id){
     unordered_map<uint64_t, node*>::iterator it = this->nodes.find(node_id);
-    //not find
+    //not finding node_id
     if(it == this->nodes.end()){
         node* new_node = new node(node_id);
-        this->nodes.insert({node_id,})
+        this->nodes.insert({node_id, new_node});
+        return 200;
     }else{
-        
+        return 204;
     }
 }
 
-
 uint64_t graph::add_edge(uint64_t node_a_id, uint64_t node_b_id){
-
+    if(node_a_id == node_b_id){
+        return 400;
+    }
+    unordered_map<uint64_t, node*>::iterator ita = this->nodes.find(node_a_id);
+    unordered_map<uint64_t, node*>::iterator itb = this->nodes.find(node_b_id);
+    if(ita == this->nodes.end() || itb == this->nodes.end()){
+        return 400;
+    }
+    unordered_set<node*> a_neighbors = ita->second->neighbors;
+    unordered_set<node*> b_neighbors = itb->second->neighbors;
+    unordered_set<node*>::iterator ita_neighbors = a_neighbors.find(node_b_id);
+    if(ita_neighbors != a_neighbors.end()){
+        return 204;
+    }
+    a_neighbors.insert(itb->second);
+    b_neighbors.insert(ita->second);
+    return 200;
 }
+
 uint64_t graph::remove_node(uint64_t node_id){
 
 }
--- A1/graph/graph.h
+++ A1/graph/graph.h
@@ -2,14 +2,22 @@
 #include "../libraray/mongoose.h"
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 
 using namespace std;
 
 struct node{
 	uint64_t id;
-	vector<node*> neighbors;
+	unordered_set<node*> neighbors;
 	node(uint64_t i){this->id=i;}
+    bool operator==(node *another) const{
+        return id == another->id;
+    }
+    bool operator==(uint64_t another_id) const{
+        return this->id == another_id;
+    }
+    
 };
 
 class graph{
@@ -18,8 +26,11 @@ private:
 public:
     //return 200 on success, 204 if the node already exists
 	uint64_t add_node(uint64_t node_id);
-    //
+    
+    //return 200 on suceess, 204 if the edge exists
+    //400 if either node does not exists, or if node_a_id = node_b_id
 	uint64_t add_edge(uint64_t node_a_id, uint64_t node_b_id);
+    
 	uint64_t remove_node(uint64_t node_id);
 	uint64_t remove_edge(uint64_t node_a_id, uint64_t node_b_id);
 	uint64_t get_node(uint64_t node_id);
