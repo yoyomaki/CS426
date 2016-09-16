92f150253ea48ffed476f612ade4a6268cd5ead6
sinexu <sx8@uw.edu>
2016-09-16 13:38:38 -0400

    手动merge之后版本

--- A1/graph/graph.cpp
+++ A1/graph/graph.cpp
@@ -1,72 +1,72 @@
 #include "graph.h"
 
-//uint64_t graph::add_node(uint64_t node_id){
-//    unordered_map<uint64_t, node*>::iterator it = this->nodes.find(node_id);
-//    if(it == this->nodes.end()){
-//        node* new_node = new node(node_id);
-//        this->nodes.insert(make_pair(node_id, new_node));
-//        return 200;
-//    }else{
-//        return 204;
-//    }
-//}
-//
-//uint64_t graph::add_edge(uint64_t node_a_id, uint64_t node_b_id){
-//    if(node_a_id == node_b_id){
-//        return 400;
-//    }
-//    unordered_map<uint64_t, node*>::iterator ita = this->nodes.find(node_a_id);
-//    unordered_map<uint64_t, node*>::iterator itb = this->nodes.find(node_b_id);
-//    if(ita == this->nodes.end() || itb == this->nodes.end()){
-//        return 400;
-//    }
-//    unordered_set<node*> a_neighbors = ita->second->neighbors;
-//    unordered_set<node*> b_neighbors = itb->second->neighbors;
-//    unordered_set<node*>::iterator ita_neighbors = a_neighbors.find(node_b_id);
-//    if(ita_neighbors != a_neighbors.end()){
-//        return 204;
-//    }
-//    a_neighbors.insert(itb->second);
-//    b_neighbors.insert(ita->second);
-//    return 200;
-//}
-//
-//uint64_t graph::remove_node(uint64_t node_id){
-//    unordered_map<uint64_t, node*>::iterator it = this->nodes.find(node_id);
-//    if(it == this->nodes.end()){
-//        return 400;
-//    }else{
-//        unordered_set<node*> nbs = it->second->neighbors;
-//        unordered_set<node*>::iterator nit;
-//        for(nit = nbs.begin(); nit < nbs.end(); ++nit){
-//            node* n = *nit;
-//            n->neighbors.erase(n->neighbors.find(node_id));
-//        }
-//        this->nodes.erase(it);
-//        return 200;
-//    }
-//}
-//
-//uint64_t graph::remove_edge(uint64_t node_a_id, uint64_t node_b_id){
-//    unordered_map<uint64_t, node*>::iterator ita = this->nodes.find(node_a_id);
-//    unordered_map<uint64_t, node*>::iterator itb = this->nodes.find(node_b_id);
-//    if(ita == this->nodes.end() || itb == this->nodes.end()){
-//        return 400;
-//    }
-//    unordered_set<node*> a_neighbors = ita->second->neighbors;
-//    unordered_set<node*> b_neighbors = itb->second->neighbors;
-//    unordered_set<node*>::iterator ita_neighbors = a_neighbors.find(node_b_id);
-//    unordered_set<node*>::iterator itb_neighbors = b_neighbors.find(node_a_id);
-//    if(ita_neighbors == a_neighbors.end()){
-//        return 400;
-//    }
-//    a_neighbors.erase(ita_neighbors);
-//    b_neighbors.erase(itb_neighbors);
-//    return 200;
-//}
+uint64_t graph::add_node(uint64_t node_id){
+    unordered_map<uint64_t, node*>::iterator it = this->nodes.find(node_id);
+    if(it == this->nodes.end()){
+        node* new_node = new node(node_id);
+        nodes.insert(make_pair(node_id, new_node));
+        return 200;
+    }else{
+        return 204;
+    }
+}
+
+uint64_t graph::add_edge(uint64_t node_a_id, uint64_t node_b_id){
+    if(node_a_id == node_b_id){
+        return 400;
+    }
+    unordered_map<uint64_t, node*>::iterator ita = this->nodes.find(node_a_id);
+    unordered_map<uint64_t, node*>::iterator itb = this->nodes.find(node_b_id);
+    if(ita == this->nodes.end() || itb == this->nodes.end()){
+        return 400;
+    }
+    unordered_map<uint64_t, node*> a_neighbors = ita->second->neighbors;
+    unordered_map<uint64_t, node*> b_neighbors = itb->second->neighbors;
+    unordered_map<uint64_t, node*>::iterator ita_neighbors = a_neighbors.find(node_b_id);
+    if(ita_neighbors != a_neighbors.end()){
+        return 204;
+    }
+    a_neighbors.insert(make_pair(node_b_id,itb->second));
+    b_neighbors.insert(make_pair(node_a_id,ita->second));
+    return 200;
+}
+
+uint64_t graph::remove_node(uint64_t node_id){
+    unordered_map<uint64_t, node*>::iterator it = this->nodes.find(node_id);
+    if(it == this->nodes.end()){
+        return 400;
+    }else{
+        unordered_map<uint64_t, node*> nbs = it->second->neighbors;
+        unordered_map<uint64_t, node*>::iterator nit;
+        for(nit = nbs.begin(); nit != nbs.end(); ++nit){
+            node* n = nit->second;
+            n->neighbors.erase(n->neighbors.find(node_id));
+        }
+        this->nodes.erase(it);
+        return 200;
+    }
+}
+
+uint64_t graph::remove_edge(uint64_t node_a_id, uint64_t node_b_id){
+    unordered_map<uint64_t, node*>::iterator ita = this->nodes.find(node_a_id);
+    unordered_map<uint64_t, node*>::iterator itb = this->nodes.find(node_b_id);
+    if(ita == this->nodes.end() || itb == this->nodes.end()){
+        return 400;
+    }
+    unordered_map<uint64_t, node*> a_neighbors = ita->second->neighbors;
+    unordered_map<uint64_t, node*> b_neighbors = itb->second->neighbors;
+    unordered_map<uint64_t, node*>::iterator ita_neighbors = a_neighbors.find(node_b_id);
+    unordered_map<uint64_t, node*>::iterator itb_neighbors = b_neighbors.find(node_a_id);
+    if(ita_neighbors == a_neighbors.end()){
+        return 400;
+    }
+    a_neighbors.erase(ita_neighbors);
+    b_neighbors.erase(itb_neighbors);
+    return 200;
+}
 
 bool graph::get_node(uint64_t node_id){
-    return this->nodes.find(node_id) != nodes.end();
+    return nodes.find(node_id) != nodes.end();
 }
 
 pair<bool,bool> graph::get_edge(uint64_t node_a_id, uint64_t node_b_id){
@@ -131,7 +131,7 @@ pair<uint64_t, bool> graph::shortest_path(uint64_t node_a_id, uint64_t node_b_id
             // If a adjacent has not been visited, then mark it visited
             // and enqueue it
             unordered_map<uint64_t, node*> a_neighbors = get_neighbors(node_a_id).second;
-            for(i = a_neighbor.begin(); i != a_neighbor.end(); ++i) {
+            for(i = a_neighbors.begin(); i != a_neighbors.end(); ++i) {
                 if(!visited[i->first]) {
                     visited[i->first] = true;
                     queue.push_back(i->first);
